a2b2bb9d606d33e1f8ef8d62635a5d513f5e62c4
Cristobal <cavila@eab.com>
2020-06-16 21:00:08 -0600

    added styles and copy button

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
 class ColorBox extends Component {
@@ -7,11 +8,19 @@ class ColorBox extends Component {
         this.state = {  }
     }
     render() {
+        const { name, background} = this.props;
         return (
-            <div style={{background: this.props.background}} className="ColorBox">
-                <span>{this.props.name}</span>
-                <span>More</span>
+            <CopyToClipboard text={background}>
+            <div style={{ background }} className="ColorBox">
+                <div className="copy-container">
+                    <div className="box-content">
+                        <span>{name}</span>
+                    </div>
+                    <button className="copy-button">Copy</button>
+                </div>
+                <span className="see-more">More</span>
             </div>
+            </CopyToClipboard>
          );
     }
 }
