08f3cf0c374c80fe196d1bdd08d728ddcf71b671
Cristobal <cavila@eab.com>
2020-06-17 19:51:33 -0600

    added copy overlay animation

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -5,21 +5,33 @@ import './ColorBox.css';
 class ColorBox extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { copied: false }
+        this.changeCopyState = this.changeCopyState.bind(this);
+    }
+    changeCopyState() {
+        this.setState({copied: true}, () => {
+            setTimeout(()=> this.setState({copied: false}), 1500);
+        });
     }
     render() {
         const { name, background} = this.props;
+        const { copied } = this.state;
         return (
-            <CopyToClipboard text={background}>
-            <div style={{ background }} className="ColorBox">
-                <div className="copy-container">
-                    <div className="box-content">
-                        <span>{name}</span>
+            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+                <div style={{ background }} className="ColorBox">
+                    <div style={{ background }} className={`copy-overlay ${copied && "show"}`}> </div>
+                        <div className={`copy-msg ${copied && "show"}`}>
+                            <h1>copied!</h1>
+                            <p>{this.props.background}</p>
+                        </div>
+                    <div className="copy-container">
+                        <div className="box-content">
+                            <span>{name}</span>
+                        </div>
+                        <button className="copy-button">Copy</button>
                     </div>
-                    <button className="copy-button">Copy</button>
+                    <span className="see-more">More</span>
                 </div>
-                <span className="see-more">More</span>
-            </div>
             </CopyToClipboard>
          );
     }
