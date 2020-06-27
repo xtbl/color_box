3ebae49f429cd78f278862149a15301dc90ff7b8
Cristobal <cavila@eab.com>
2020-06-27 16:08:47 -0600

    add palette route

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import Palette from './Palette';
 import { generatePalette } from "./colorHelpers";
 
 function App() {
-  console.log(generatePalette(seedColors[4]));
+  const findPalette = function(id) {
+    return seedColors.find(function(palette){
+      return palette.id === id;
+    });
+  };
   return (
     <Switch>
       <Route exact path="/" render={() => <h1>PALETTE LIST</h1>} />
-      <Route exact path="/palette/:id" render={() => <h1>INDIVIDUAL PALETTE</h1>} />
+      <Route exact
+        path="/palette/:id"
+        render={routeProps => <Palette palette={generatePalette(findPalette(routeProps.match.params.id))}/>} />
     </Switch>
-    // <div className="App">
-    //   <Palette palette={generatePalette(seedColors[4])}/>
-    // </div>
   );
 }
 
